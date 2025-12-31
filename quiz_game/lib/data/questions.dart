a0cd4bbf8feed24ebb71b9c7f4fd080962fed377
Daniel Opara <oparadaniv@gmail.com>
2026-01-01 00:39:42 +0100

    adding questions

---
+++ quiz_game/lib/data/questions.dart
@@ -0,0 +1,34 @@
+import 'package:quiz_game/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion('Which planet is known as the Red Planet?', [
+    'Mars',
+    'Venus',
+    'Jupiter',
+    'Saturn',
+  ]),
+  QuizQuestion('What programming language is primarily used with Flutter?', [
+    'Dart',
+    'Java',
+    'Kotlin',
+    'Swift',
+  ]),
+  QuizQuestion('Which operating system is developed by Linus Torvalds?', [
+    'Linux',
+    'Windows',
+    'macOS',
+    'Unix',
+  ]),
+  QuizQuestion('What does CPU stand for?', [
+    'Central Processing Unit',
+    'Computer Personal Unit',
+    'Central Power Unit',
+    'Control Processing Utility',
+  ]),
+  QuizQuestion('Which database is commonly used with Django by default?', [
+    'SQLite',
+    'MongoDB',
+    'Redis',
+    'Firebase',
+  ]),
+];
