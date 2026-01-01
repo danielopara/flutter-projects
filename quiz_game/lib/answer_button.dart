36dd49a0064a461bc05a3975a0b387808c2cf449
Daniel Opara <oparadaniv@gmail.com>
2026-01-01 22:25:53 +0100

    adding a reusable button for the custom button

---
+++ quiz_game/lib/answer_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    required this.answerText,
+    required this.onTap,
+    super.key,
+  });
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(foregroundColor: Colors.black),
+        child: Text(
+          answerText,
+          style: TextStyle(fontSize: 20),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
