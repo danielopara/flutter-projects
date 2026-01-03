018636f0788856d1990235a3905500089a1be06f
Daniel Opara <oparadaniv@gmail.com>
2026-01-03 19:04:53 +0100

    summary data widget

--- quiz_game/lib/answer_screen.dart
+++ quiz_game/lib/answer_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_game/data/questions.dart';
 import 'package:quiz_game/summary.dart';
 
 class AnswerScreen extends StatelessWidget {
-  const AnswerScreen({super.key, required this.selectedAnswers});
+  const AnswerScreen({
+    super.key,
+    required this.selectedAnswers,
+    required this.restartQuiz,
+  });
 
   final List<String> selectedAnswers;
+  final void Function() restartQuiz;
 
   List<Map<String, Object>> getSummaryData() {
     final List<Map<String, Object>> summary = [];
@@ -18,12 +24,17 @@ class AnswerScreen extends StatelessWidget {
         'selected_answer': selectedAnswers[i],
       });
     }
-    print(selectedAnswers);
     return summary;
   }
 
   @override
   Widget build(BuildContext context) {
+    var summaryData = getSummaryData();
+    var numOfQuestions = questions.length;
+    var numOfCorrectAnswers = summaryData.where((data) {
+      return data['selected_answer'] == data['correct_answer'];
+    }).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -31,10 +42,33 @@ class AnswerScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Answers'),
+            Text(
+              'You answered $numOfCorrectAnswers out of $numOfQuestions questions correctly!',
+              style: GoogleFonts.lato(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
             const SizedBox(height: 30),
             Summary(summaryData: getSummaryData()),
-            TextButton(onPressed: () {}, child: Text('reset quiz')),
+            const SizedBox(height: 10),
+            TextButton.icon(
+              onPressed: () {
+                restartQuiz();
+              },
+              icon: Icon(Icons.refresh_sharp),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                iconSize: 16,
+              ),
+              label: Text(
+                'Restart quiz',
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+            ),
           ],
         ),
       ),
--- quiz_game/lib/summary.dart
+++ quiz_game/lib/summary.dart
@@ -4,25 +4,79 @@ class Summary extends StatelessWidget {
   const Summary({super.key, required this.summaryData});
   final List<Map<String, Object>> summaryData;
 
+  Color getNumberColor(Map<String, Object> data) {
+    return data['correct_answer'] == data['selected_answer']
+        ? Colors.green
+        : Colors.red;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: summaryData.map((data) {
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(((data['question_index'] as int) + 1).toString()),
-            Column(
-              children: [
-                Text(data['question_text'] as String),
-                const SizedBox(height: 5),
-                Text((data['selected_answer'] as String)),
-                Text((data['correct_answer'] as String)),
-              ],
+    return Center(
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 500),
+        child: SizedBox(
+          height: 300,
+          child: SingleChildScrollView(
+            child: Column(
+              children: summaryData.map((data) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 29,
+                      height: 28,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: getNumberColor(data),
+                      ),
+                      child: Text(
+                        ((data['question_index'] as int) + 1).toString(),
+                        style: TextStyle(),
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    Expanded(
+                      child: Column(
+                        // textDirection: TextDirection.ltr,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            data['question_text'] as String,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                            textAlign: TextAlign.start,
+                          ),
+                          const SizedBox(height: 5),
+                          Text(
+                            (data['selected_answer'] as String),
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 234, 231, 231),
+                            ),
+                          ),
+                          Text(
+                            (data['correct_answer'] as String),
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 44, 236, 51),
+                              // fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              }).toList(),
             ),
-          ],
-        );
-      }).toList(),
+          ),
+        ),
+      ),
     );
   }
 }
