6b3c3509833d6463036fdcbdf85c207122767064
Daniel Opara <oparadaniv@gmail.com>
2026-01-05 21:37:02 +0100

    adding in enum

--- expense_tracker/lib/model/expense.dart
+++ expense_tracker/lib/model/expense.dart
@@ -2,12 +2,19 @@ import 'package:uuid/uuid.dart';
 
 final uuid = Uuid();
 
+enum Category { food, leisure, transportation, data }
+
 class Expense {
-  Expense({required this.title, required this.amount, required this.date})
-    : id = uuid.v4();
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.category,
+    required this.date,
+  }) : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
