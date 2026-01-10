816824589ec5fc6533d36f7f2091f4656cbb279c
Daniel Opara <oparadaniv@gmail.com>
2026-01-10 06:25:13 +0100

    creating the expense list widget and rendering the content to the main page

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/model/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,9 +12,35 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _expenseList = [
+    Expense(
+      amount: 20000,
+      title: 'Food',
+      category: Category.food,
+      date: DateTime.now(),
+    ),
+    Expense(
+      amount: 1000,
+      title: 'Leisure',
+      category: Category.leisure,
+      date: DateTime(2024, 1, 1),
+    ),
+    Expense(
+      amount: 5000,
+      title: 'Transportation',
+      category: Category.transportation,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('chart'),
+          Expanded(child: ExpensesList(expenses: _expenseList)),
+        ],
+      ),
+    );
   }
 }
