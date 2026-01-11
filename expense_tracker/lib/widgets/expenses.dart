4018f55e62fe3c5f127e7ab9f855200d8693bad9
Daniel Opara <oparadaniv@gmail.com>
2026-01-11 20:45:05 +0100

    modal functionality

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expense_list/expenses_list.dart';
 import 'package:expense_tracker/model/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -37,7 +38,7 @@ class _ExpensesState extends State<Expenses> {
     showModalBottomSheet(
       context: context,
       builder: (ctx) {
-        return Text('Hello');
+        return NewExpense();
       },
     );
   }
