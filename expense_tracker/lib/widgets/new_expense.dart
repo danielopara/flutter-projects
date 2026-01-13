7a29c53e61b01182293a882dcbb52c51a29cd4eb
Daniel Opara <oparadaniv@gmail.com>
2026-01-13 20:52:34 +0100

    adding amount field and navigator.pop()

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -10,15 +10,53 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsetsGeometry.all(16),
       child: Column(
         children: [
-          const TextField(
+          TextField(
             maxLength: 50,
-            decoration: InputDecoration(label: Text('Title')),
+            controller: _titleController,
+            decoration: InputDecoration(label: const Text('Title')),
+          ),
+          TextField(
+            maxLength: 10,
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              prefix: const Text('₦ '),
+              label: const Text('Amount'),
+            ),
+          ),
+
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
+                child: const Text('Save Expense'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+            ],
           ),
         ],
       ),
