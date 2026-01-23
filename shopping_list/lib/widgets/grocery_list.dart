6eeb91fe77ffad9a356386941e61e1740af031df
Daniel Opara <oparadaniv@gmail.com>
2026-01-23 05:34:19 +0100

    added _removeItem function and made the lIst tile dismissible

--- shopping_list/lib/widgets/grocery_list.dart
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -40,6 +40,29 @@ class _GroceryListState extends State<GroceryList> {
     );
   }
 
+  void _removeItem(GroceryItem item) {
+    final groceryItemIndex = _groceryItems.indexOf(item);
+    setState(() {
+      _groceryItems.remove(item);
+    });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text('Item delete'),
+        duration: Duration(seconds: 5),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _groceryItems.insert(groceryItemIndex, item);
+            });
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget content = _groceryItems.isEmpty
@@ -48,14 +71,20 @@ class _GroceryListState extends State<GroceryList> {
           )
         : ListView.builder(
             itemCount: _groceryItems.length,
-            itemBuilder: (ctx, index) => ListTile(
-              title: Text(_groceryItems[index].name),
-              leading: Container(
-                width: 24,
-                height: 24,
-                color: _groceryItems[index].category.color,
+            itemBuilder: (ctx, index) => Dismissible(
+              onDismissed: (direction) {
+                _removeItem(_groceryItems[index]);
+              },
+              key: ValueKey(_groceryItems[index].id),
+              child: ListTile(
+                title: Text(_groceryItems[index].name),
+                leading: Container(
+                  width: 24,
+                  height: 24,
+                  color: _groceryItems[index].category.color,
+                ),
+                trailing: Text('${_groceryItems[index].quantity}'),
               ),
-              trailing: Text('${_groceryItems[index].quantity}'),
             ),
           );
 
