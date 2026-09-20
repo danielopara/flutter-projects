94f63927c51d051afea060d9dc027781cfc1ea78
Daniel Opara <oparadaniv@gmail.com>
2026-09-20 08:03:00 +0100

    fine tuning

--- fd_calculator/lib/main.dart
+++ fd_calculator/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:pattern_formatter/pattern_formatter.dart';
 
 void main() {
   runApp(MaterialApp(home: HomePage()));
@@ -16,6 +19,7 @@ class _HomePageState extends State<HomePage> {
   final TextEditingController _amount = TextEditingController();
   final TextEditingController _interestRate = TextEditingController();
   final TextEditingController _months = TextEditingController();
+  final _money = NumberFormat('#,##0.00');
   double? interest;
   double? total;
 
@@ -28,7 +32,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   void calculate() {
-    final amount = double.tryParse(_amount.text);
+    final amount = double.tryParse(_amount.text.replaceAll(',', ''));
     final interestRate = double.tryParse(_interestRate.text);
     final period = int.tryParse(_months.text);
 
@@ -70,112 +74,110 @@ class _HomePageState extends State<HomePage> {
 
   Widget body() {
     return SingleChildScrollView(
-      child: Container(
-        color: Colors.grey[300],
-        child: Column(
-          children: [
-            Container(
-              height: 170,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 0, 84, 152),
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(100),
-                ),
+      child: Column(
+        children: [
+          Container(
+            height: 170,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 0, 84, 152),
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(100),
               ),
-              child: Padding(
-                padding: EdgeInsets.only(bottom: 20),
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Fixed Deposit',
-                        style: GoogleFonts.robotoMono(
-                          fontSize: 35,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
+            ),
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 20),
+              child: Align(
+                alignment: Alignment.center,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Fixed Deposit',
+                      style: GoogleFonts.robotoMono(
+                        fontSize: 35,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
                       ),
-                      Text(
-                        'Calculator',
-                        style: GoogleFonts.robotoMono(
-                          fontSize: 35,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
+                    ),
+                    Text(
+                      'Calculator',
+                      style: GoogleFonts.robotoMono(
+                        fontSize: 35,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
-            SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.fromLTRB(30, 10, 40, 0),
-              child: Column(
-                children: [
-                  input(
-                    title: 'Amount',
-                    controller: _amount,
-                    hint: 'Enter Amount',
-                  ),
-                  input(
-                    title: 'Rate %',
-                    controller: _interestRate,
-                    hint: 'Enter interest',
-                  ),
-                  input(
-                    title: 'Months',
-                    controller: _months,
-                    hint: 'Enter Period (in months)',
-                  ),
-                  SizedBox(height: 10),
-                  GestureDetector(
-                    onTap: () {
-                      calculate();
-                    },
-                    child: Container(
-                      height: 60,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 0, 84, 152),
-                        borderRadius: BorderRadius.circular(25),
-                      ),
-                      child: Center(
-                        child: Text(
-                          'CALCULATE',
-                          style: GoogleFonts.robotoMono(
-                            color: Colors.white,
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                          ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.fromLTRB(30, 10, 40, 0),
+            child: Column(
+              children: [
+                input(
+                  title: 'Amount',
+                  controller: _amount,
+                  formatters: [ThousandsFormatter(allowFraction: true)],
+                  hint: 'Enter Amount',
+                ),
+                input(
+                  title: 'Rate %',
+                  controller: _interestRate,
+                  hint: 'Enter interest',
+                ),
+                input(
+                  title: 'Months',
+                  controller: _months,
+                  hint: 'Enter Period (in months)',
+                ),
+                SizedBox(height: 10),
+                GestureDetector(
+                  onTap: () {
+                    calculate();
+                  },
+                  child: Container(
+                    height: 60,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 0, 84, 152),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'CALCULATE',
+                        style: GoogleFonts.robotoMono(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
                   ),
-                  SizedBox(height: 10),
-                  interest == null
-                      ? SizedBox()
-                      : Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Interest: NGN ${interest!.toStringAsFixed(2)}",
-                              style: GoogleFonts.robotoMono(fontSize: 20),
-                            ),
-                            Text(
-                              "Total: NGN ${total!.toStringAsFixed(2)}",
-                              style: GoogleFonts.robotoMono(fontSize: 20),
-                            ),
-                          ],
-                        ),
-                ],
-              ),
+                ),
+                SizedBox(height: 10),
+                interest == null
+                    ? SizedBox()
+                    : Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Interest: NGN ${_money.format(interest!)}",
+                            style: GoogleFonts.robotoMono(fontSize: 20),
+                          ),
+                          Text(
+                            "Total: NGN ${_money.format(total!)}",
+                            style: GoogleFonts.robotoMono(fontSize: 20),
+                          ),
+                        ],
+                      ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -184,6 +186,7 @@ class _HomePageState extends State<HomePage> {
     required String title,
     required TextEditingController controller,
     required String hint,
+    List<TextInputFormatter>? formatters,
   }) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -191,14 +194,23 @@ class _HomePageState extends State<HomePage> {
         Text(title, style: GoogleFonts.robotoMono(fontSize: 16)),
         SizedBox(height: 20),
         Container(
-          height: 60,
+          height: 50,
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(20),
           ),
           child: TextField(
             controller: controller,
+            inputFormatters: formatters,
+            keyboardType: TextInputType.numberWithOptions(decimal: true),
             decoration: InputDecoration(
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide(
+                  color: Color.fromARGB(255, 0, 84, 152),
+                  width: 2.5,
+                ),
+              ),
               border: OutlineInputBorder(borderSide: BorderSide.none),
               hint: Text(hint),
             ),
